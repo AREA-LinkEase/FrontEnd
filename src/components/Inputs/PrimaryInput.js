907256cec6a25c319a8c1d12c525ad831cb417e3
Adil Nouiri <91637808+Nabzilooo@users.noreply.github.com>
2023-12-05 16:15:14 +0100

    Add component primary input

---
+++ src/components/Inputs/PrimaryInput.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { colors } from '../../style/color';
+import * as Icon from 'react-feather';
+
+const PrimaryInput = ({
+  inputValue,
+  setInputValue,
+  borderColor = colors.lightGrey,
+  backgroundColor = colors.lightGrey,
+  borderRadius = '15px',
+  placeholder = 'Email',
+  placeholderColor = colors.darkGrey,
+  textFont = 'Arial, sans-serif',
+  textSize = '20px',
+  textColor = 'black',
+  inputType = 'email',
+  leftIconName = 'User',
+  leftIconSize = '25px',
+  leftIconColor = colors.darkBlue,
+  rightIconName,
+  rightIconSize = '25px',
+  rightIconColor = colors.darkBlue,
+}) => {
+  const LeftIcon = leftIconName ? Icon[leftIconName] : null;
+  const RightIcon = rightIconName ? Icon[rightIconName] : null;
+
+  return (
+    <div
+      style={{
+        display: 'inline-block',
+        border: `1px solid ${borderColor}`,
+        backgroundColor: backgroundColor,
+        borderRadius: borderRadius,
+        padding: '10px',
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}
+      >
+        {LeftIcon && (
+          <div style={{ paddingRight: '10px', marginBottom: '-4px' }}>
+            {React.createElement(LeftIcon, {
+              size: leftIconSize,
+              color: leftIconColor,
+            })}
+          </div>
+        )}
+        <input
+          placeholder={placeholder}
+          type={inputType}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          style={{
+            borderWidth: 0,
+            backgroundColor: backgroundColor,
+            color: inputValue ? textColor : placeholderColor,
+            fontSize: textSize,
+            fontFamily: textFont,
+            outline: 'none',
+          }}
+        />
+        {RightIcon && (
+          <div style={{ paddingLeft: '10px', marginBottom: '-4px' }}>
+            {React.createElement(RightIcon, {
+              size: rightIconSize,
+              color: rightIconColor,
+            })}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PrimaryInput;
