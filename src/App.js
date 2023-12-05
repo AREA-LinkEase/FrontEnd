2edb2f4381bc35e8d73f731ef4379ed5840f6c6e
Adil Nouiri <91637808+Nabzilooo@users.noreply.github.com>
2023-12-05 16:42:24 +0100

    Init dev branch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import { useEffect, useState } from 'react';
-import PrimaryInput from './components/Inputs/PrimaryInput';
 
 const App = () => {
-
-  const [inputEmailValue, setInputEmailValue] = useState('');
-
-  useEffect(() => {
-    console.log(inputEmailValue);
-  }, [inputEmailValue])
-
+  
   return (
     <div>
-      <PrimaryInput inputValue={inputEmailValue} setInputValue={setInputEmailValue} />
     </div>
   );
 }
