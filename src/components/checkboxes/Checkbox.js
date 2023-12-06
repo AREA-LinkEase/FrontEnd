62363d35f142cb9fb6849fae270e0fe6bbd9ae7e
Adil Nouiri <91637808+Nabzilooo@users.noreply.github.com>
2023-12-06 19:12:14 +0100

    Add text description next to checkbox

--- src/components/checkboxes/Checkbox.js
+++ src/components/checkboxes/Checkbox.js
@@ -10,19 +10,39 @@ const Checkbox = ({
   borderWidth = '2px',
   width = '30px',
   height = '30px',
+	text,
+  textColor = colors.darkBlue,
+	textFont = 'Arial, sans-serif',
+  textSize = '15px',
 }) => {
 
   return (
-    <div
-			onClick={() => setIsChecked(!isChecked)}
-    	style={{
-        backgroundColor: backgroundColor,
-        borderColor: borderColor,
-        border: `${borderWidth} solid ${borderColor}`,
-        borderRadius: borderRadius,
-        width: width,
-        height: height
-    }}/>
+		<div style={{
+			display: 'flex',
+			flexDirection: 'row',
+			alignItems: 'center'
+		}}>
+			<div
+				onClick={() => setIsChecked(!isChecked)}
+				style={{
+					backgroundColor: backgroundColor,
+					borderColor: borderColor,
+					border: `${borderWidth} solid ${borderColor}`,
+					borderRadius: borderRadius,
+					width: width,
+					height: height,
+					cursor: 'pointer'
+			}}/>
+			<div style={{paddingLeft: text !== '' ? '10px' : '0px'}}>
+				<span style={{
+					color: textColor,
+					fontFamily: textFont,
+					fontSize: textSize
+				}}>
+					{text}
+				</span>
+			</div>
+		</div>
   );
 };
 
