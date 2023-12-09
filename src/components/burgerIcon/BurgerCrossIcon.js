fb936148a73676fb541d2c270a6f2df354f11f71
Adil Nouiri <91637808+Nabzilooo@users.noreply.github.com>
2023-12-09 15:55:24 +0100

    Fix animation burgerCross

--- src/components/burgerIcon/BurgerCrossIcon.js
+++ src/components/burgerIcon/BurgerCrossIcon.js
@@ -6,14 +6,15 @@ const BurgerCrossIcon = () => {
 
   const handleClick = () => {
     setIsOpen(!isOpen);
+    console.log("lool");
   };
 
   return (
     <div className={`${styles.container} ${isOpen ? styles['nav-open'] : ''}`}>
       <a id="menu-toggle" className={styles['menu-toggle']} onClick={handleClick}>
-        <span className={styles['menu-toggle-bar']}></span>
-        <span className={styles['menu-toggle-bar']}></span>
-        <span className={styles['menu-toggle-bar']}></span>
+        <span className={`${styles['menu-toggle-bar']} ${styles['menu-toggle-bar--top']}`}></span>
+        <span className={`${styles['menu-toggle-bar']} ${styles['menu-toggle-bar--middle']}`}></span>
+        <span className={`${styles['menu-toggle-bar']} ${styles['menu-toggle-bar--bottom']}`}></span>
       </a>
     </div>
   );
