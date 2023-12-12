5d4e993d59b94535d04f2917f23f10b514f17426
thomp <thomas.papaix@epitech.eu>
2023-12-12 15:47:43 +0100

    [ADD] (LoginPage) all front

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -1,25 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import "./LoginPage.css";
 import PrimaryInput from "../../components/Inputs/PrimaryInput";
 import PrimaryButton from "../../components/buttons/primaryButton";
+import IconButton from "../../components/buttons/IconButton";
+import {svgs} from "../../style/svgs/svgList";
 
 const LoginPage = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     return (
         <div className={"loginBody"}>
             <div className={"loginContainer"}>
                 <div className={"loginInput"}>
-                    <PrimaryInput placeholder={"Username"} />
-                    <PrimaryInput placeholder={"Password"} leftIconName={"Lock"}/>
-
+                    <PrimaryInput placeholder={"Username"} width={"85%"} inputValue={username} setInputValue={setUsername}/>
+                    <PrimaryInput placeholder={"Password"} inputType={"Password"} leftIconName={"Lock"} width={"85%"} setInputValue={setPassword} inputValue={password} />
                 </div>
                 <PrimaryButton buttonText={"Login"} width={"95%"}/>
             </div>
             <div className={"loginSeparation"}>
                 <div className={"loginSeparationLine"}></div>
-                <div className={"loginSeparationText"}>Or connect with</div>
+                <p className={"loginSeparationText"}>Or connect with</p>
                 <div className={"loginSeparationLine"}></div>
             </div>
-
+            <div className={"loginSocialMedia"}>
+                <IconButton width={"90%"}/>
+                <IconButton width={"90%"} buttonText={"Microsoft"} iconSrc={svgs.microsoft}/>
+            </div>
+            <div className={"loginSingUp"}>
+                <p className={"loginSingUpText"}>Don't have an account?</p>
+                <p className={"loginSingUpTextLink"}>Sign Up</p>
+            </div>
         </div>
     );
 };
