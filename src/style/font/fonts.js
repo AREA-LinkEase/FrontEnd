b1c383a8f02226ecc671643cdb991ff7436c8133
Adil Nouiri <91637808+Nabzilooo@users.noreply.github.com>
2023-12-12 16:21:33 +0100

    Add openSans font

--- src/style/font/fonts.js
+++ src/style/font/fonts.js
@@ -1,5 +1,6 @@
 export const fonts = {
     avenirNextLTProRegular: 'avenirNextLTProRegular',
     avenirNextLTProIt: 'avenirNextLTProIt',
-    avenirNextLTProBold: 'avenirNextLTProBold'
+    avenirNextLTProBold: 'avenirNextLTProBold',
+    openSans: 'Open Sans'
 };
