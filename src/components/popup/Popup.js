cac1924c334b06440c98534dd9a86ccd04515736
Adil Nouiri <91637808+Nabzilooo@users.noreply.github.com>
2023-12-15 21:27:07 +0100

    [FEAT](page): add complete version of workspace edit page

--- src/components/popup/Popup.js
+++ src/components/popup/Popup.js
@@ -1,43 +1,50 @@
-import React from "react";
-import "./Popup.css";
+import React, { useRef, useEffect } from "react";
+import styles from "./Popup.module.css";
 import PropTypes from "prop-types";
 import PrimaryButton from "../buttons/PrimaryButton";
 import H1Text from "../texts/H1Text";
 import PText from "../texts/PText";
-import {colors} from "../../style/color";
+import { colors } from "../../style/color";
 
-const Modal = ({
-                   Title = "Title",
-                   Content = "Content",
-                   TextButton = "TextButton",
-                   state = false,
-                   onPress = "",
-               }) => {
-    return (
-        <div className={"popupBody"}>
-            {state && (
-                <div onClick={state} className="popupOverlay">
-                    <div className="popupContent">
-                        <H1Text text={Title} size={"30px"} color={colors.black}/>
-                        <div className={"popupText"}>
-                            <PText text={Content} size={"15px"}/>
-                        </div>
-                        <div className="popupButton">
-                            <PrimaryButton buttonText={TextButton} width={"300%"} textSize={"80%"} height={"47px"} onPressButton={onPress}/>
-                        </div>
-                    </div>
-                </div>
-            )}
+const Popup = ({ Title = "Title", Content = "Content", TextButton = "TextButton", onPress, leavePopup }) => {
+  const popupRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (popupRef.current && !popupRef.current.contains(event.target)) {
+        leavePopup();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [leavePopup]);
+
+  return (
+    <div className={styles.popupBody}>
+      <div className={styles.popupOverlay}/>
+      <div className={styles.popupContent} ref={popupRef}>
+        <H1Text text={Title} size={"28px"} color={colors.lightBlack} />
+        <div className={styles.popupText}>
+          <PText text={Content} size={"14px"} color={colors.black} />
         </div>
-    );
-}
-
-Modal.propTypes = {
-    Title: PropTypes.string,
-    Content: PropTypes.string,
-    TextButton: PropTypes.string,
-    state: PropTypes.bool,
-    onPress: PropTypes.string,
+        <div className={styles.popupButton}>
+          <PrimaryButton buttonText={TextButton} width={"300%"} textSize={"80%"} height={"47px"} onPressButton={onPress} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Modal;
+Popup.propTypes = {
+  Title: PropTypes.string,
+  Content: PropTypes.string,
+  TextButton: PropTypes.string,
+  onPress: PropTypes.func.isRequired,
+  leavePopup: PropTypes.func.isRequired,
+};
+
+export default Popup;
--- src/pages/workspace/WorkspaceEdit.js
+++ src/pages/workspace/WorkspaceEdit.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 import styles from "./WorkspaceEdit.module.css";
 import Header from "../../components/Header";
@@ -8,47 +8,90 @@ import { fontWeights } from "../../style/font/fontWeights";
 import { fonts } from "../../style/font/fonts";
 import PrimaryInput from "../../components/Inputs/PrimaryInput";
 import BottomNavbar from "../../components/navbar/BottomNavbar";
+import Popup from "../../components/popup/Popup";
+import IconButton from "../../components/buttons/IconButton";
 
 const WorkspaceEdit = ({id, name, description}) => {
     const [nameValue, setNameValue] = useState(name);
     const [descriptionValue, setDescriptionValue] = useState(description);
     const [isOpenPopup, setIsOpenPopup] = useState(false);
+    const [isModified, setIsModified] = useState(false);
+    const [initialName, setInitialName] = useState(name);
+    const [initialDescription, setInitialDescription] = useState(description);
 
-    const handleClickDelete = () => {
-        console.log("Clicked on deleted workspace");
+    useEffect(() => {
+        setInitialName(name);
+        setInitialDescription(description);
+        setIsModified(false);
+    }, [name, description]);
+
+    // const handleSaveChanges = () => {
+    //     setIsModified(false);
+    // }
+
+    const handleNameChange = (newValue) => {
+        setNameValue(newValue);
+        setIsModified(newValue !== initialName || descriptionValue !== initialDescription);
+    }
+
+    const handleDescriptionChange = (newValue) => {
+        setDescriptionValue(newValue);
+        setIsModified(nameValue !== initialName || newValue !== initialDescription);
+    }
+
+    // Fonctions d'ouverture / fermerture popup
+    const handleClickDeleteText = () => {
         setIsOpenPopup(!isOpenPopup);
     };
 
-	return (
-		<div key={id} className={styles.workspaceBody}>
-			<Header rightIconColor={colors.white} leftIconSize="30px"/>
-            <div style={{paddingLeft: '20px', paddingRight: '20px', width: '100%'}}>
-                <div style={{paddingBottom: '60px'}}>
+    const handleClosePopup = () => {
+        console.log("Clicked on screen to close popup");
+        setIsOpenPopup(false);
+    };
+
+    const handleClickDeleteButttonPopup = () => {
+        console.log("Clicked on button delete workspace");
+        setIsOpenPopup(false);
+    }
+
+    return (
+        <div key={id} className={styles.workspaceEditBody}>
+            <Header rightIconColor={colors.white} leftIconSize="30px"/>
+            <div style={{width: '100%'}}>
+                <div style={{paddingBottom: '60px', paddingLeft: '20px', paddingRight: '20px'}}>
                     <PText text='Workspace parameters' fontWeight={fontWeights.bold} font={fonts.openSans} color={colors.lightBlack} size="21px"/>
                 </div>
-                <div style={{paddingBottom: '20px'}}>
+                <div style={{paddingBottom: '20px', paddingLeft: '20px'}}>
                     <PText text='Edit the name :' font={fonts.openSans} color={colors.lightBlack} size="17px"/>
                 </div>
-                <div style={{paddingBottom: '60px'}}>
-                    <PrimaryInput width="90%" leftIconName='' fontWeight={fontWeights.normal} placeholder="Name" inputType="text" textSize="15px" inputValue={nameValue} setInputValue={setNameValue}/>
+                <div style={{paddingBottom: '60px', paddingLeft: '20px'}}>
+                    <PrimaryInput width="90%" leftIconName='' fontWeight={fontWeights.normal} placeholder="Name" inputType="text" textSize="15px" inputValue={nameValue} setInputValue={handleNameChange}/>
                 </div>
-                <div style={{paddingBottom: '20px'}}>
+                <div style={{paddingBottom: '20px', paddingLeft: '20px'}}>
                     <PText text='Edit the description:' font={fonts.openSans} color={colors.lightBlack} size="17px"/>
                 </div>
-                <div style={{paddingBottom: '100px'}}>
-                    <PrimaryInput width="90%" leftIconName='' isTextArea={true} placeholder="Description..." rows={8} fontWeight={fontWeights.normal} inputType="text" textSize="15px" inputValue={descriptionValue} setInputValue={setDescriptionValue}/>
+                <div style={{paddingBottom: '100px', paddingLeft: '20px'}}>
+                    <PrimaryInput width="90%" leftIconName='' isTextArea={true} placeholder="Description..." rows={8} fontWeight={fontWeights.normal} inputType="text" textSize="15px" inputValue={descriptionValue} setInputValue={handleDescriptionChange}/>
                 </div>
-                <div style={{width: "91%", textAlign: 'center'}}>
+                <div style={{width: "100%", textAlign: 'center'}}>
                     <div
-                        onClick={handleClickDelete}
+                        onClick={handleClickDeleteText}
                         style={{cursor: 'pointer', display: 'inline-block'}}>
                         <PText text='Delete the workspace' font={fonts.openSans} color='red' fontWeight={fontWeights.bold} size="15px"/>
                     </div>
                 </div>
-                <BottomNavbar/>
+                { isOpenPopup && (
+                    <Popup onPress={handleClickDeleteButttonPopup} leavePopup={handleClosePopup} Title="Are you sure to delete it ?!" Content="The workspace and his content will definitely be deleted." TextButton="Delete" />
+                )}
+                { isModified && (
+                    <div style={{position: 'fixed', bottom: 75, width: '100%', textAlign: 'center'}}>
+                        <IconButton width="90%" height="60px" buttonText='Add' iconSrc='' backgroundColor={colors.darkPurple} textColor={colors.white} hoverBackgroundColor={colors.darkPurple} />
+                    </div>
+                )}
+                <BottomNavbar isPopupVisible={isOpenPopup}/>
             </div>
-		</div>
-	);
+        </div>
+    );
 };
 
 WorkspaceEdit.propTypes = {
