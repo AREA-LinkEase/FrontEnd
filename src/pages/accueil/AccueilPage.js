381bc8b68594ae0a9cf799f992100d99651c4b44
Adil Nouiri <91637808+Nabzilooo@users.noreply.github.com>
2023-12-15 11:28:07 +0100

    [FIX](component): norm eslint

--- src/pages/accueil/AccueilPage.js
+++ src/pages/accueil/AccueilPage.js
@@ -14,11 +14,11 @@ const AccueilPage = () => {
     return (
         <div className={styles.accueilBody}>
           <div className={styles.accueilH1Text}>
-				  	<H1Text text="Let's go !" color={colors.lightlightGrey}/>
+				    <H1Text text="Let's go !" color={colors.lightlightGrey}/>
           </div>
           <div className={styles.accueilCenterDiv}>
             <div className={styles.accueilPText}>
-					    <PText text={parseTextWithLineBreaks(texts.travailQuotidien)} color={colors.lightlightGrey}/>
+              <PText text={parseTextWithLineBreaks(texts.travailQuotidien)} color={colors.lightlightGrey}/>
             </div>
 					  <PrimaryButton buttonText='Create' width='90%' height='65px'/>
           </div>
