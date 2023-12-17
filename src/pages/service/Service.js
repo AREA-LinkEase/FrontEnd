b1458b2d456e80a95b9e0d48d86ff4c97b4cd7be
Thomas PAPAIX <91823510+Thomaspapaix@users.noreply.github.com>
2023-12-17 16:28:44 +0100

    [Feature] (Servive) add all css and scroll

--- src/pages/service/Service.js
+++ src/pages/service/Service.js
@@ -1,14 +1,15 @@
-import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types";
+import * as Icon from 'react-feather';
+import React, {useState} from "react";
+import {colors} from "../../style/color";
 import styles from "./Service.module.css";
 import Header from "../../components/Header";
 import H1Text from "../../components/texts/H1Text";
 import BottomNavbar from "../../components/navbar/BottomNavbar";
-import PrimaryButton from "../../components/buttons/PrimaryButton";
-import {colors} from "../../style/color";
-import TitleTextChildButton from "../../components/buttons/TitleTextChildButton";
 import SwitchButton from "../../components/switches/SwitchButton";
+import PrimaryButton from "../../components/buttons/PrimaryButton";
 import adjustColorBrightness from "../../utils/adjustColorBrightness";
+import TitleTextChildButton from "../../components/buttons/TitleTextChildButton";
 
 
 const Service = ({
@@ -17,13 +18,20 @@ const Service = ({
     logo = "https://via.placeholder.com/71",
     description = "Description du service je suis la description du service et je raconte que fait ce service et aussi comment il peut ettre utilise dans linkease"
                  }) => {
-    const [workspaceAccessValue, setWorkspaceAccessValue] = useState("All");
-    const [workspaceList] = useState([{
+
+    const [showIcon, setShowIcon] = useState(false);
+    const Settings = Icon['Settings'];
+    const handleClick = () => {
+        setShowIcon(!showIcon);
+    };
+
+    const [BoxAccessValue, setBoxAccessValue] = useState("All");
+    const [boxList] = useState([{
         name: 'SpotifyBangar',
         creator: 'Adilou le fifou',
         people: 3500000,
         color: colors.lightPurple,
-        access: "Public"
+        access: "Automate"
     },
         {
             name: 'Baboss',
@@ -37,113 +45,126 @@ const Service = ({
             creator: 'Adilou le fifou',
             people: 3500000,
             color: colors.lightPurple,
-            access: "Public"
+            access: "Automate"
         },
         {
             name: '3ataï',
             creator: 'Adilou le fifou',
             people: 3500000,
             color: colors.lightlightGrey,
-            access: "Public"
+            access: "Automate"
         },
         {
             name: 'THOAAAAMS',
             creator: 'Adilou le fifou',
             people: 3500000,
             color: colors.purple,
-            access: "Public"
+            access: "Workspace"
         },
         {
             name: 'PIZZA BIEN GARNIE',
             creator: 'Adilou le fifou',
             people: 3500000,
             color: colors.purple,
-            access: "Private"
+            access: "Workspace"
         }]);
 
-    const [numberItems, setNumberItems] = useState({
-        "All": 0,
-        "Private": 0,
-        "Public": 0,
-    });
-
-    useEffect(() => {
-        const countWorkspaceTypes = () => {
-            const counts = {
-                "All": 0,
-                "Private": 0,
-                "Public": 0,
-            };
-
-            workspaceList.forEach((workspace) => {
-                counts.All += 1;
-                counts[workspace.access] += 1;
-            });
-
-            setNumberItems(counts);
-        }
-        countWorkspaceTypes();
-    }, [workspaceList]);
   return (
     <div>
-        <div style={{background: color, height: "318px"}}>
+        <div style={{
+            background: color,
+            height: "318px",
+            position: 'fixed',
+            zIndex: 2000, top: '0',
+            width: '100%'
+        }}>
             <div className={styles.serviceBackground}>
                 <img src={logo} alt="logo" />
+                {showIcon &&
+                    <div
+                    style={{cursor: 'pointer',
+                        marginLeft: 'auto',
+                        marginRight: '-12px',
+                        zIndex: "3000",
+                        position: "fixed",
+                        top: "57px",
+                        left: "87%"
+                    }}>
+                    {React.createElement(Settings, {
+                        size: '24px',
+                        color: colors.white,
+                    })}
+                </div>}
                 <H1Text text={title} size={"24px"}/>
                 <p className={styles.serviceDescription}>{description}</p>
-                <PrimaryButton text="Connect" backgroundColor={"white"} hoverBackgroundColor={"gray"} width={"45%"} height={"42px"}/>
+                <PrimaryButton
+                    buttonText={"Connect"}
+                    textSize={"15px"}
+                    backgroundColor={"white"}
+                    hoverBackgroundColor={"#CFCFCF"}
+                    width={"35%"} height={"42px"}
+                    borderColor={"gray"}
+                    textColor={"black"}
+                    onPressButton={handleClick}
+                />
             </div>
         </div>
         <div className={styles.serviceFilter}>
-            <Header isFilter={true} filterListTag={["All", "Workspace", "Automate"]} />
+            <Header isFilter={true}
+                    filterListTag={["All", "Workspace", "Automate"]}
+                    filterSelected={BoxAccessValue}
+                    setFilterSelected={setBoxAccessValue}
+            />
         </div>
-        <div className={styles.serviceList}>
-            {workspaceList.length !== 0 ? (
-                <div className={styles.homeWorkspaceList}>
-                    {workspaceList
-                        .filter(
-                            (workspace) =>
-                                workspaceAccessValue === "All" ||
-                                workspace.access
-                                    .toLowerCase()
-                                    .includes(workspaceAccessValue.toLowerCase())
-                        )
-                        .map((workspace, index, filteredList) => (
-                            <div
-                                key={index}
-                                style={{
-                                    paddingBottom:
-                                        index === filteredList.length - 1 ? '90px' : '15px',
-                                }}
-                            >
-                                <TitleTextChildButton
-                                    title={workspace.name}
-                                    text={`Par **${workspace.creator}**`}
-                                    isSelectable={false}
-                                    componentId={index}
-                                    isClickable={true}
-                                    backgroundColor={workspace.access === 'Public' ? workspace.color : '#777777'}
-                                    borderColor={workspace.access === 'Public' ? workspace.color : '#777777'}
-                                    width="90%"
-                                    ComponentChildren={() => (
-                                        <SwitchButton
-                                            textColorOff={colors.white}
-                                            backgroundColorOn={adjustColorBrightness(workspace.color, -50)}
-                                            colorOn={workspace.color}
-                                            textColorOn={colors.white}
-                                            isOn={workspace.access === "Private" ? false : true}
-                                            isLittle={true}
-                                            width="120px"
-                                            height="20px"
-                                        />
-                                    )}
-                                />
-                            </div>
-                        ))}
-                </div>
-            ) : null}
-        </div>
-
+            <div
+                className={styles.serviceContainer}
+                style={{ marginTop: '85px', paddingTop: '295px'}}
+            >
+                {boxList.length !== 0 ? (
+                    <div className={styles.serviceList}>
+                        {boxList
+                            .filter(
+                                (box) =>
+                                    BoxAccessValue === "All" ||
+                                    box.access
+                                        .toLowerCase()
+                                        .includes(BoxAccessValue.toLowerCase())
+                            )
+                            .map((box, index, filteredList) => (
+                                <div
+                                    key={index}
+                                    style={{
+                                        paddingBottom:
+                                            index === filteredList.length - 1 ? '90px' : '15px',
+                                    }}
+                                >
+                                    <TitleTextChildButton
+                                        title={box.name}
+                                        text={`Par **${box.creator}**`}
+                                        isSelectable={false}
+                                        componentId={index}
+                                        isClickable={true}
+                                        backgroundColor={box.access === 'Public' ? box.color : '#777777'}
+                                        borderColor={box.access === 'Public' ? box.color : '#777777'}
+                                        width="90%"
+                                        ComponentChildren={() => (
+                                            <SwitchButton
+                                                textColorOff={colors.white}
+                                                backgroundColorOn={adjustColorBrightness(box.color, -50)}
+                                                colorOn={box.color}
+                                                textColorOn={colors.white}
+                                                isOn={box.access!=="Private"}
+                                                isLittle={true}
+                                                width="120px"
+                                                height="20px"
+                                            />
+                                        )}
+                                    />
+                                </div>
+                            ))}
+                    </div>
+                ) : null}
+            </div>
         <BottomNavbar />
     </div>
   );
