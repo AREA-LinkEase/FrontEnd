87ebf234f7186d042b78f9169ee7eb25fac75f7e
thomp <thomas.papaix@epitech.eu>
2023-12-18 13:42:09 +0100

    [FIX] (Register) backgroundColor & files name

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,19 +1,20 @@
 import React, {useState} from "react";
 import PropTypes from "prop-types";
-import "./RegisterEmail.css"
+import styles from "./Register.module.css";
 import Header from "../../components/Header";
 import H1Text from "../../components/texts/H1Text";
 import PrimaryInput from "../../components/Inputs/PrimaryInput";
 import PrimaryButton from "../../components/buttons/PrimaryButton";
+import {colors} from "../../style/color";
 
-const RegisterEmail = ({
+const Register = ({
     Title = "Quel est ton email ?",
     InputPlaceholder = "No place holder",
     leftIconSrc = "",
     rightIconSrcOn = "",
     rightIconSrcOff = "",
     ButtonText = "no text",
-    inputType = "password",
+    inputType = "email",
     }) => {
     const [inputValue, setInputValue] = useState('');
     const [isActive, setIsActive] = useState(inputType !== 'password');
@@ -25,22 +26,22 @@ const RegisterEmail = ({
         inputType === "email" ? "" : isActive ? rightIconSrcOn : rightIconSrcOff;
 
     return (
-        <div className={"registerEmailBody"}>
+        <div className={styles.registerEmailBody}>
             <Header rightIconName={""}/>
-            <div className={"registerTitle"}>
-                <H1Text text={Title} fontWeight={"550"} size={"38px"}/>
+            <div className={styles.registerTitle}>
+                <H1Text text={Title} fontWeight={"550"} size={"38px"} color={colors.black}/>
             </div>
-            <div className={"registerInput"}>
+            <div className={styles.registerInput}>
                 <PrimaryInput rightIconName={rightIconName} inputType={isActive ? 'email' : 'password'} isRightIconIsActive={isActive} onPressRightIcon={onPressRightIcon} inputValue={inputValue} setInputValue={setInputValue} placeholder={InputPlaceholder} leftIconName={leftIconSrc} />
             </div>
-            <div className={"registerButton"}>
+            <div className={styles.registerButton}>
                 <PrimaryButton buttonText={ButtonText} width={"90%"}/>
             </div>
         </div>
     );
 }
 
-RegisterEmail.propTypes = {
+Register.propTypes = {
     Title: PropTypes.string,
     InputPlaceholder: PropTypes.string,
     leftIconSrc: PropTypes.string,
@@ -50,4 +51,4 @@ RegisterEmail.propTypes = {
     inputType: PropTypes.string,
 };
 
-export default RegisterEmail;
+export default Register;
