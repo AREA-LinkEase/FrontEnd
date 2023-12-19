a22fb5340363f2430b4bc401b75e8f48e2a2a4e7
SimonVermeulen <simon.vermeulen@epitech.eu>
2023-12-19 20:23:41 +0100

    [Feat](Models): send request

--- src/models/sendRequest.js
+++ src/models/sendRequest.js
@@ -2,14 +2,10 @@ import { API_LINK } from "./config";
 
 export async function sendRequest(url, opts) {
     const result = await fetch(API_LINK + url, opts)
-    if (result.status >= 400)
-        throw "error " + result.status;
-    return result.json();
-}
+    let content = await result.json()
 
-export async function sendRequestImage(url, opts) {
-    const result = await fetch(API_LINK + url, opts)
-    if (result.status >= 400)
-        throw "error " + result.status;
-    return result;
+    return {
+        content: content,
+        status: result.status
+    };
 }
