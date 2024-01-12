6f804a333c56ba6146298ef9421ca9562c000766
IKEA.luddes <clement.pecas33@gmail.com>
2024-01-12 12:35:49 +0100

    [Feat] (automate) : link logs with API

--- src/views/automates/LogsTable.js
+++ src/views/automates/LogsTable.js
@@ -10,62 +10,53 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import CustomTextField from "../../@core/components/mui/text-field";
 import {DataGrid} from "@mui/x-data-grid";
+import NetworkConfig from "../../configs/networkConfig";
+import Avatar from "@mui/material/Avatar";
+import {useRouter} from "next/router";
 
 const defaultColumns = [
     {
         flex: 0.2,
-        field: 'title',
+        field: 'value',
         minWidth: 90,
-        headerName: 'Title',
-        renderCell: ({ row }) => <Typography sx={{ color: 'text.secondary' }}>{row.title}</Typography>
+        headerName: 'Value',
+        renderCell: ({ row }) => <Typography sx={{ color: 'text.secondary' }}>{row.value}</Typography>
     },
     {
         flex: 0.15,
         minWidth: 80,
-        field: 'value',
-        headerName: 'Value',
-        renderCell: ({ row }) => <Typography sx={{ color: 'text.secondary' }}>{row.value}</Typography>
+        field: 'user',
+        headerName: 'User',
+        renderCell: ({ row }) => <Avatar src={NetworkConfig.url + "/assets/avatars/" + row.userId + ".png"} alt={row.userId} sx={{ mr: 4, width: 30, height: 30 }} />
     },
     {
         flex: 0.25,
         minWidth: 90,
         field: 'type',
         headerName: 'Type',
-        renderCell: ({ row }) => <CustomChip rounded label={row.type} skin='light' color={row.type} />
+        renderCell: ({ row }) => <CustomChip rounded label={row.status} skin='light' color={row.status} />
     }
 ]
 
-const LogsTable = () => {
+const LogsTable = ({data, automate}) => {
     const [value, setValue] = useState('')
-    const [show, setShow] = useState(false)
     const [paginationModel, setPaginationModel] = useState({ page: 0, pageSize: 6 })
 
     const columns = [
-        ...defaultColumns,
-        {
-            flex: 0.1,
-            minWidth: 130,
-            sortable: false,
-            field: 'actions',
-            headerName: 'Actions',
-            renderCell: ({ row }) => (
-                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                    <Tooltip title='Delete Automate'>
-                        <IconButton size='small' onClick={() => {}}>
-                            <Icon icon='tabler:trash' />
-                        </IconButton>
-                    </Tooltip>
-                </Box>
-            )
-        }
+        ...defaultColumns
     ]
+
+    const router = useRouter()
 
     return (
         <Card>
             <CardContent
                 sx={{ gap: 4, display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}
             >
-                <Button variant='contained' color={"error"} onClick={() => setShow(true)}>
+                <Button variant='contained' color={"error"} onClick={async () => {
+                  await automate.clearLogs()
+                  router.reload()
+                }}>
                     Clear logs
                 </Button>
                 <Box sx={{ gap: 4, display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}>
@@ -75,14 +66,7 @@ const LogsTable = () => {
             <DataGrid
                 autoHeight
                 rowHeight={54}
-                rows={[
-                    {
-                        "id": 0,
-                        "title": "test",
-                        "value": "value",
-                        "type": "error"
-                    }
-                ]}
+                rows={data}
                 columns={columns}
                 disableRowSelectionOnClick
                 paginationModel={paginationModel}
